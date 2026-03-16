b91de233a6dd373339a774cb6d9b816983ae3a97
mckayhyorgason <mckay.yorgason21@gmail.com>
2026-03-16 13:52:37 -0600

    required on models

--- backend/Mission10_Yorgason/Mission10_Yorgason/Models/Bowler.cs
+++ backend/Mission10_Yorgason/Mission10_Yorgason/Models/Bowler.cs
@@ -1,27 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Mission10_Yorgason.Models;
 
 public partial class Bowler
 {
     public int BowlerId { get; set; }
+    [Required]
 
-    public string? BowlerLastName { get; set; }
+    public string BowlerLastName { get; set; }
+    [Required]
 
-    public string? BowlerFirstName { get; set; }
+    public string BowlerFirstName { get; set; }
 
-    public string? BowlerMiddleInit { get; set; }
+    public string BowlerMiddleInit { get; set; }
+    [Required]
 
-    public string? BowlerAddress { get; set; }
+    public string BowlerAddress { get; set; }
+    [Required]
 
-    public string? BowlerCity { get; set; }
+    public string BowlerCity { get; set; }
+    [Required]
 
-    public string? BowlerState { get; set; }
+    public string BowlerState { get; set; }
+    [Required]
 
-    public string? BowlerZip { get; set; }
+    public string BowlerZip { get; set; }
+    [Required]
 
-    public string? BowlerPhoneNumber { get; set; }
+    public string BowlerPhoneNumber { get; set; }
 
     public int? TeamId { get; set; }
 
